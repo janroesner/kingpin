ed90dd0ec395406e9d82f5bdbb6683865126f1ee
Jan Roesner <jan@roesner.it>
2011-03-10 11:50:01 +0100

    is_alive? and shutdown work as expected. Signed-off-by: Jan Roesner <jan@roesner.it>

--- lib/pin_caster.rb
+++ lib/pin_caster.rb
@@ -1,7 +1,24 @@
 class Pincaster
 
+  @@http_client = HttpClient.new('http','localhost',4269)
+
+  # Pincaster server is stil alive?
   def self.is_alive?
-    HttpClient.new('http', 'localhost', 4269).send_request('GET', '/api/1.0/system/ping.json').code == "200" ? true : false rescue false
+    @@http_client.send_request('GET', '/api/1.0/system/ping.json').code == "200" ? true : false rescue false
+  end
+
+  # shutdown Pincaster server immediately
+  def self.shutdown!
+    begin
+      @@http_client.send_request('POST', '/api/1.0/system/shutdown.json')
+    rescue Exception => e
+      case e.message
+      when "end of file reached"
+        return true
+      else
+        return false
+      end
+    end
   end
 
 end
