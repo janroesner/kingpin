718dc5b465cd1f6ca74644f5bf090161ae68a99e
Jan Roesner <jan@roesner.it>
2011-03-10 16:38:24 +0100

    Stuck with DEG/RAD issue, have to rethink. Signed-off-by: Jan Roesner <jan@roesner.it>

--- lib/pin.rb
+++ lib/pin.rb
@@ -24,13 +24,34 @@ module PinInstanceMethods
     Pincaster.add_record(self)
   end
 
+  # add a Pincaster record for self with layer: self.class BUT self's lng and lat are in RAD not DEG
+  def add_pin_rad
+    Pincaster.add_record_rad(self)
+  end
+
+  # returns the Pincaster pin for self
   def pin
     Pincaster.get_record(self)
   end
 
+  # deletes the Pincaster pin for self
+  def delete_pin!
+    Pincaster.delete_record(self)
+  end
+
+  # returns nearby Pincaster pins in given radius of self
+  def nearby(radius)
+    Pincaster.nearby(self, radius)
+  end
+
+  # returns nearby Pincaster pins in given radius of self BUT self's lat and lng are in RAD not DEG
+  def nearby_rad(radius)
+    Pincaster.nearby(self, radius, rad=true)
+  end
+
   # returns objects longitude
   def pin_lng
-    [:longitude, :long, :lng].each do |l|
+    [:longitude, :long, :lng, :lgt, :lgdt].each do |l|
       return self.send(l) if self.respond_to?(l)
     end
     return nil
@@ -38,7 +59,7 @@ module PinInstanceMethods
 
   # returns objects latitude
   def pin_lat
-    [:latitude, :lati, :ltt, :lat].each do |l|
+    [:latitude, :lati, :ltt, :ltd, :lat].each do |l|
       return self.send(l) if self.respond_to?(l)
     end
     return nil
